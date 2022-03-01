5172622e808ffbd5fb54e32fc7654352ed8d2700
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-01 13:59:30 +0100

    creating the hero section
    creating new component  and implementing svg logo

---
+++ src/components/heroPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="menu">
+      <div class="site-logo" w-50px h-50px mt-10px ml-10px />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.site-logo {
+    background-image: url("../../public/logo.svg");
+    background-size: cover;
+}
+</style>
