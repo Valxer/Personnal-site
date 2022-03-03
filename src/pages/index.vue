dd382d5a8bef26c1a1e167fcd499cb2414cab1fe
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-03 11:17:19 +0100

    sett scroll to smooth

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import AboutPage from '~/components/AboutPage.vue'
 </script>
 
 <template>
   <router-view />
   <HeroPage />
+  <AboutPage />
 </template>
 
 <style>
+html{
+  scroll-behavior: smooth;
+}
 main{
   padding: 0 !important
 }
