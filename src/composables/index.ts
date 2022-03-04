db051829e25f4be9967ce8b6ab5a132f0c19d693
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-04 22:03:16 +0100

    creating contactScroll function

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -3,3 +3,7 @@ export function aboutScroll() {
   const about = document.querySelector('.about-section')
   about?.scrollIntoView()
 }
+export function contactScroll() {
+  const contact = document.querySelector('contact-section')
+  contact?.scrollIntoView()
+}
