797e97158718fc18ffc07174b2c3fda97f53ea71
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-05 16:43:08 +0100

    navbar enhancement
    - blurry background on display
    - animation in and out
    -fixed position on mobile to always have the menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,28 +10,30 @@ function toggleNavBar() {
 </script>
 
 <template>
-  <div class="menu" absolute top-0 w-screen flex flex-col justify-between items-start>
+  <div class="menu" absolute top-0 w-screen flex justify-between items-start>
     <div class="site-logo" w-50px h-50px ml-25px mt-25px />
-    <a href="#" class="toggle-navbar" flex flex-col justify-between w-30px h-21px @click="toggleNavBar()">
-      <span class="slice" />
-      <span class="slice" />
-      <span class="slice" />
-    </a>
-    <div class="navbar" flex justify-end w-full mr-25px mt-15px>
-      <ul w-125px mr-25px flex flex-col justify-between items-end>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+    <div class="nav-container w-4/10" relative h-screen min-w-125px>
+      <button href="#" class="toggle-navbar z-2" fixed top-0 flex flex-col justify-between w-30px h-21px @click="toggleNavBar()">
+        <span class="slice" />
+        <span class="slice" />
+        <span class="slice" />
+      </button>
+      <nav class="navbar z-1" fixed flex justify-end w-full h-full>
+        <ul class="navlist h-1/3" w-full min-w-125px min-h-300px flex flex-col justify-between items-start pt-100px pl-25px>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">PROJECTS</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -42,7 +44,6 @@ function toggleNavBar() {
   background-size: cover;
 }
 .toggle-navbar {
-    position: absolute;
     top: 2.5rem;
     right: 25px;
 }
@@ -53,13 +54,22 @@ function toggleNavBar() {
     border-radius: 10px;
 }
 .navbar {
-    display: none;
-  &.active{
-      display: flex;
-}
+    background: hsl(0 0% 100% / 0.1);
+    backdrop-filter: blur(1rem);
+    @media screen and (max-width: 640px) {
+
+      transform: translateX(100%);
+      transition: 500ms ease-out;
+      &.active{
+          display: flex;
+          transform: translateX(0%);
+          transition: 500ms ease-out;
+      }
+    }
 }
 li{
     margin-bottom: 3px;
+    cursor: pointer;
 }
 @media screen and (min-width: 640px) {
     .toggle-navbar{
@@ -74,10 +84,23 @@ li{
     .site-logo{
         margin: 0;
     }
+    .nav-container {
+      height: 100px;
+      width: 75%;
+      max-width: 650px;
+    }
     .navbar{
-        width: 400px;
+        position: static;
+        background: none;
+        backdrop-filter: none;
+        width: 100%;
         display: flex;
         margin-top: 0px;
+    }
+    .navlist {
+      padding: 0;
+      justify-content: space-around;
+      min-height: 100px;
     }
     ul{
         width: 100%;
