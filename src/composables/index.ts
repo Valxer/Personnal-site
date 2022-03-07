7fdaa299c374ee58b3fa98bd1d0813abb4e89726
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-07 11:38:16 +0100

    corrected function

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,9 +1,11 @@
 export * from './dark'
 export function aboutScroll() {
   const about = document.querySelector('.about-section')
+  console.log("about : ", about)
   about?.scrollIntoView()
 }
 export function contactScroll() {
-  const contact = document.querySelector('contact-section')
+  const contact = document.querySelector('.contact-section')
+  console.log("contact : ", contact)
   contact?.scrollIntoView()
 }
