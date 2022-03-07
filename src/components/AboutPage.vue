1f7257cbaa61241ff5d97858c5c0060a7770a19a
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-07 19:49:36 +0100

    animation on btn plus margin fix

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -6,14 +6,14 @@ import { contactScroll } from '~/composables'
 
 <template>
   <section id="about-section" class="about-section" relative w-screen flex flex-col justify-center items-center text-true-gray600>
-    <div class="content h-9/10" flex flex-col justify-start items-center mt-50px>
-      <h2 class="header" flex flex-col justify-between items-center mb-30px>
-        <span class="header__title" relative text-fuchsia-500 text-lg font-semibold text-center mb-15px>ABOUT ME</span>
+    <div class="content h-9/10" flex flex-col justify-start items-center mt-40px>
+      <h2 class="header" flex flex-col justify-between items-center mb-50px>
+        <span class="header__title" relative text-fuchsia-500 text-lg font-semibold text-center mb-30px>ABOUT ME</span>
         <span class="header__text w-6/10" text-sm font-600 leading-tight>Get to know more about me and my current skills</span>
       </h2>
       <div class="details" w-full flex flex-col justify-between items-center text-left>
         <div class="details__main w-7/10" mb-30px>
-          <h3 class="details__main-title" text-true-gray800 font-600 text-xl mb-15px>
+          <h3 class="details__main-title" text-true-gray800 font-600 text-xl mb-25px>
             Hello !
           </h3>
           <div class="details__main-details" text-justify text-sm>
@@ -24,17 +24,17 @@ import { contactScroll } from '~/composables'
             <p class="details__main-details-para">
               I love to be up-to-date about the tools I'm using especially my preferred framework : <strong>Vue</strong>.
             </p>
-            <p class="details__main-details-para" mt-10px>
+            <p class="details__main-details-para" mt-25px>
               I'm curently working on my portfolio in order to display my skills to any person interested in seeing what I'm capable of.
             </p>
-            <p class="details__main-details-para" mt-10px>
+            <p class="details__main-details-para" mt-25px>
               I would love to hear from you. Whether it's a <strong>project</strong>, <strong>job</strong> opportunity,
               helpful <strong>criticism</strong> about my work or just a chat. Feel free to <strong>contact</strong> me!
             </p>
           </div>
         </div>
         <div class="details__skills w-7/10">
-          <h3 class="details__skills-title" text-dark-700 font-600 text-xl mb-20px>
+          <h3 class="details__skills-title" text-dark-700 font-600 text-xl mb-40px>
             My Skills
           </h3>
           <div class="skills" flex flex-wrap justify-center>
@@ -71,7 +71,7 @@ import { contactScroll } from '~/composables'
           </div>
         </div>
       </div>
-      <button class="contact" mt-40px mb-50px px-30px py-10px border-2 border-fuchsia600 text-fuchsia600 text-lg font-semibold @click="contactScroll">
+      <button class="contact-btn" mt-40px mb-50px px-30px py-10px border-2 border-fuchsia600 text-fuchsia600 text-lg font-semibold @click="contactScroll">
         CONTACT
       </button>
     </div>
@@ -102,11 +102,18 @@ import { contactScroll } from '~/composables'
     border-radius: 5px;
     background-color: rgb(153, 153, 153, 0.5);
 }
+.contact-btn {
+  transition: 300ms ease;
+  &:hover {
+    transform: scale(1.3);
+    transition: 300ms ease;
+  }
+}
 @media screen and (min-width: 1024px) {
   .about-section {
     background-image: url("/About.svg");
     background-repeat: no-repeat;
-    background-size: 20%;
+    background-size: 19%;
     background-position: 100% 15%;
     @media screen and (min-width: 1350px) {
       background-position: 100% 50%;
@@ -124,6 +131,7 @@ import { contactScroll } from '~/composables'
       margin-top: 15px;
       flex-direction: row;
       justify-content: space-between;
+      align-items: flex-start;
       width: 80%;
       @media screen and (min-width:1350px) {
               width: 60%
