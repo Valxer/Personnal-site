19ee1bb54a29f1c3d0696c38724427b620d142d6
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-08 16:29:42 +0100

    basic display of projects

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
+
+import { goTo, projects } from '~/composables'
+
 </script>
 
 <template>
-  <div class="card-container">
-    <div class="card-image" />
+  <div v-for="project in projects" :key="project.name" class="card-container w-7/10 " relative flex flex-col justify-between items-left text-left mb-50px>
+    <img :src="project.image" class="card-image">
     <h3 class="card-title">
-      Titre raccoleur
+      {{ project.name }}
     </h3>
     <p class="card-excerpt">
-      Description aguicheuse de ouf
+      {{ project.excerpt }}
     </p>
-    <button class="card-btn">
+    <button class="card-btn" @click="goTo(project.link)">
       LIEN TENTANT
     </button>
   </div>
