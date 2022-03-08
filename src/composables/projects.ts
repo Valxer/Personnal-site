5b2901aef0ddf06e9c27af527eb523ed8ec41797
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-08 16:28:37 +0100

    updating interface
    setting up project interface to populate my project section

--- src/composables/projects.ts
+++ src/composables/projects.ts
@@ -1,34 +1,36 @@
-interface Project {
+export interface Project {
   name: string
   excerpt: string
   skills?: any
   image?: string
   link?: string
+  livelink?: string
 }
 
 const project1: Project = {
   name: 'Personnal Website',
-  excerpt: '',
+  excerpt: 'The site you are currently on. Used to display my projects and skills to anyone interested in them!',
   skills: ['VUE', 'HTML', 'SASS', 'Typescript', 'TailwindCSS', 'GIT', 'Github'],
+  image: '/public/Website.webp',
   link: 'https://levanphungk.com/',
+  livelink: 'hhtps://levanphungk.com/',
 }
 const project2: Project = {
   name: 'Munchkin Character Sheet',
   excerpt: 'A personnal application used to create characters and follow their progreesion in  the Munchkin board game.',
   skills: ['VUE', 'HTML', 'SASS', 'Javascript', 'GIT', 'Github'],
-  link: 'https://levanphungk.com/',
+  link: 'https://munchkincharsheet.netlify.app/',
+  livelink: 'https://munchkincharsheet.netlify.app/',
 }
 const project3: Project = {
   name: 'Oui',
   excerpt: '',
   skills: ['VUE', 'HTML', 'SASS', 'Typescript', 'TailwindCSS', 'GIT', 'Github'],
-  link: 'https://levanphungk.com/',
 }
 const project4: Project = {
   name: 'Non',
   excerpt: '',
   skills: ['VUE', 'HTML', 'SASS', 'Typescript', 'TailwindCSS', 'GIT', 'Github'],
-  link: 'https://levanphungk.com/',
 }
 
 export const projects: Project[] = []
