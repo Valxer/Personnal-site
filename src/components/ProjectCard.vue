1c487d5a19a1cf1de828ee2966e91d79b784dabf
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-08 15:21:05 +0100

    Basic display without css

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card-container">
+    <div class="card-image" />
+    <h3 class="card-title">
+      Titre raccoleur
+    </h3>
+    <p class="card-excerpt">
+      Description aguicheuse de ouf
+    </p>
+    <button class="card-btn">
+      LIEN TENTANT
+    </button>
+  </div>
+</template>
+
+<style scoped lang="sccs">
+</style>
