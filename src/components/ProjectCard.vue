1ce7f03ed5d29d23b9081fc52b6a96537c6ee919
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-15 14:21:19 +0100

    styling project cards
    added hover animation for project cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -12,28 +12,105 @@ import { goTo, projects } from '~/composables'
     relative
     flex
     flex-col
-    justify-between
-    items-left
+    items-center
     text-left
-    mb-50px
+    mb-75px
   >
-    <img :src="project.image" class="card-image">
-    <h3 class="card-title">
-      {{ project.name }}
-    </h3>
-    <p class="card-excerpt">
-      {{ project.excerpt }}
-    </p>
-    <div class="skills" flex flex-wrap justify-center>
-      <div v-for="skill in project.skills" :key="skill" class="skill">
-        {{ skill }}
+    <img
+      :src="project.image"
+      class="card-image"
+      absolute
+      top-0
+      left-0
+      w-full
+      rounded-xl
+      border-2
+      border-true-gray-500
+    >
+    <div
+      class="card-content"
+      w-full
+      flex
+      flex-col
+      justify-evenly
+      items-end
+      rounded-xl
+      border-2
+      border-true-gray-500
+      bg-true-gray-200
+      pr-20px
+    >
+      <h3 class="card-title w-15/20" font-bold text-xl text-dark-700>
+        {{ project.name }}
+      </h3>
+      <p class="card-excerpt  w-15/20">
+        {{ project.excerpt }}
+      </p>
+      <div class="skills w-15/20" flex flex-wrap justify-start>
+        <div v-for="skill in project.skills" :key="skill" class="skill">
+          {{ skill }}
+        </div>
+      </div>
+      <div
+        class="btn-container w-15/20"
+        flex
+        justify-start
+        text-dark-700
+        text-lg
+      >
+        <button
+          class="card-btn"
+          mr-30px
+          px-15px
+          py-5px
+          border-2
+          border-fuchsia600
+          font-semibold
+          @click="goTo(project.livelink)"
+        >
+          LIVE VERSION
+        </button>
+        <button
+          class="card-btn"
+          px-15px
+          py-5px
+          border-2
+          border-fuchsia600
+          font-semibold
+          @click="goTo(project.link)"
+        >
+          SEE MORE
+        </button>
       </div>
     </div>
-    <button class="card-btn" @click="goTo(project.link)">
-      SEE LIVE
-    </button>
   </div>
 </template>
 
-<style scoped lang="sccs">
+<style scoped lang="scss">
+.card-content, .card-image {
+    aspect-ratio: 16/9;
+}
+.card-image {
+  transition: 300ms ease-out;
+}
+.card-container:hover .card-image {
+  border-width: 5px;
+  transform: scale(0.4) translateX(-125%);
+  transition: 300ms ease-out;
+}
+.skills {
+  gap: 10px;
+}
+.skill{
+    padding: 2px 10px;
+    border-radius: 5px;
+    background-color: rgb(153, 153, 153, 0.5);
+}
+.card-btn {
+  transition: 300ms ease;
+  &:hover {
+    transform: scale(1.2);
+    transition: 300ms ease;
+  }
+}
 </style>
