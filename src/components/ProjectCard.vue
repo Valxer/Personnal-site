ff5091a4037d3cfd6b69b896e549924be8e7be2e
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-15 15:40:35 +0100

    added responsive card display

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,83 +5,85 @@ import { goTo, projects } from '~/composables'
 </script>
 
 <template>
-  <div
-    v-for="project in projects"
-    :key="project.name"
-    class="card-container w-7/10 "
-    relative
-    flex
-    flex-col
-    items-center
-    text-center sm:text-left
-    mb-75px
-    rounded-xl
-    border-2 sm:border-0
-    border-true-gray-500
-    bg-true-gray-200
-  >
-    <img
-      :src="project.image"
-      class="card-image w-9/10"
-      top-0
-      left-0
-      mt-25px sm:mt-0
-      sm:w-full
-      sm:h-full
-      rounded-xl
-      border-2
-      border-true-gray-500
-    >
+  <div class="main-container" w-full flex flex-wrap flex-col xl:flex-row xl:justify-evenly items-center>
     <div
-      class="card-content"
-      w-full
+      v-for="project in projects"
+      :key="project.name"
+      class="card-container w-7/10 xl:w-38/100"
+      relative
       flex
       flex-col
-      justify-evenly
-      items-center sm:items-end
-      sm:rounded-xl
-      sm:border-2
+      items-center
+      text-center sm:text-left
+      mb-75px
+      rounded-xl
+      border-2 sm:border-0
       border-true-gray-500
-      py-25px
-      sm:pr-20px
+      bg-true-gray-200
     >
-      <h3 class="card-title w-15/20" font-bold text-xl text-dark-700>
-        {{ project.name }}
-      </h3>
-      <p class="card-excerpt  w-15/20">
-        {{ project.excerpt }}
-      </p>
-      <div class="skills w-15/20" flex flex-wrap justify-center sm:justify-start>
-        <div v-for="skill in project.skills" :key="skill" class="skill">
-          {{ skill }}
+      <img
+        :src="project.image"
+        class="card-image w-9/10"
+        top-0
+        left-0
+        mt-25px sm:mt-0
+        sm:w-full xl
+        sm:h-full
+        rounded-xl
+        border-2
+        border-true-gray-500
+      >
+      <div
+        class="card-content"
+        w-full
+        flex
+        flex-col
+        justify-evenly
+        items-center sm:items-end
+        sm:rounded-xl
+        sm:border-2
+        border-true-gray-500
+        py-25px
+        sm:pr-20px
+      >
+        <h3 class="card-title w-15/20" font-bold text-xl text-dark-700>
+          {{ project.name }}
+        </h3>
+        <p class="card-excerpt  w-15/20">
+          {{ project.excerpt }}
+        </p>
+        <div class="skills w-15/20" flex flex-wrap justify-center sm:justify-start>
+          <div v-for="skill in project.skills" :key="skill" class="skill">
+            {{ skill }}
+          </div>
         </div>
-      </div>
-      <div class="btn-container w-15/20" flex justify-center sm:justify-start text-dark-700 text-sm sm:text-lg mt-10px>
-        <button
-          v-show="project.livelink"
-          class="card-btn"
-          mr-20px
-          px-10px sm:px-15px
-          py-5px
-          border-2
-          border-fuchsia600
-          font-semibold
-          @click="goTo(project.livelink)"
-        >
-          LIVE VERSION
-        </button>
-        <button
-          v-show="project.link"
-          class="card-btn"
-          px-15px
-          py-5px
-          border-2
-          border-fuchsia600
-          font-semibold
-          @click="goTo(project.link)"
-        >
-          GITHUB LINK
-        </button>
+        <div class="btn-container w-15/20" flex justify-center sm:justify-start text-dark-700 text-sm sm:text-lg mt-10px>
+          <button
+            v-show="project.livelink"
+            class="card-btn"
+            mr-20px
+            px-10px sm:px-15px
+            py-5px
+            border-2
+            border-fuchsia600
+            font-semibold
+            @click="goTo(project.livelink)"
+          >
+            LIVE VERSION
+          </button>
+          <button
+            v-show="project.link"
+            class="card-btn"
+            px-15px
+            py-5px
+            border-2
+            border-fuchsia600
+            font-semibold
+            @click="goTo(project.link)"
+          >
+            GITHUB LINK
+          </button>
+        </div>
       </div>
     </div>
   </div>
