0f1c676551fd18e20299fa67da679106fbb32b5d
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-16 13:49:45 +0100

    glasmorphism + tilt

--- src/components/ProjectPage.vue
+++ src/components/ProjectPage.vue
@@ -5,6 +5,7 @@
   <section
     id="project-section"
     class="project-section"
+    relative
     w-screen
     h-screeen
     flex
@@ -13,7 +14,7 @@
     items-center
     text-true-gray600
   >
-    <h2 class="header" flex flex-col justify-between items-center mb-50px mt-40px lg:mt-140px>
+    <h2 class="header z-1" flex flex-col justify-between items-center mb-50px mt-40px lg:mt-140px>
       <span class="header__title" relative text-fuchsia-500 text-2xl font-semibold text-center mb-30px>
         PROJECTS
       </span>
@@ -28,7 +29,33 @@
 
 <style scoped lang="scss">
 .project-section {
+  overflow: hidden;
   background-color: #FFFFFF;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -20%;
+    left: -35%;
+    width: 100%;
+    height:100%;
+    background: linear-gradient(#46efd9, #d946ef);
+    clip-path: circle();
+    @media screen and (min-width: 1024px) {
+      width: 75%;
+      height: 75%;
+      top: 1%;
+    }
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    width: 90%;
+    height: 90%;
+    background: linear-gradient(#d946ef, #efd946);
+    clip-path: circle();
+    bottom: -20%;
+    right:-35%;
+  }
 }
 .header__title::after {
     content: '';
