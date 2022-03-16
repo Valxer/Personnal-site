76b820effb60726fd5e959e3a66edab2d360a834
Kevin Le Van Phung <levanphungk@gmail.com>
2022-03-16 10:39:09 +0100

    positioning and sizing loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="loader-container z-5" w-screen h-screen absolute bg-true-gray700>
+    <span class="ring" />
+    <span class="ring" />
+    <span class="ring" />
+  </div>
+</template>
